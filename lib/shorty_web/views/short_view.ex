852b29cfc164f3945d05008a9c798c79ac3112a8
Michel Belleville <michel.belleville@gmail.com>
2020-07-29 17:08:33 +0200

    view code a little dry-er with helpers

--- lib/shorty_web/views/short_view.ex
+++ lib/shorty_web/views/short_view.ex
@@ -1,3 +1,11 @@
 defmodule ShortyWeb.ShortView do
   use ShortyWeb, :view
+
+  def link_to_short(conn, short) do
+    link_to Routes.page_url(conn, :to_full_url, short.hash_id)
+  end
+
+  def link_to(url) do
+    link url, to: url
+  end
 end
