210a4a7fe0032029a4b4d49d9e2551126565767a
Michel Belleville <michel.belleville@gmail.com>
2020-07-29 13:39:07 +0200

    brand new phoenix app

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :shorty,
+  ecto_repos: [Shorty.Repo]
+
+# Configures the endpoint
+config :shorty, ShortyWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "aSgQ61mn0nGiwkYJKV4O7Pk5hcXBB0t/WuWZA3MhS/yjgFLQWO6aXqJXM2C+PgcY",
+  render_errors: [view: ShortyWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Shorty.PubSub,
+  live_view: [signing_salt: "a4UhPsV8"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
